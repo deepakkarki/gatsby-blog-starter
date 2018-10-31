36cb131fa01d55621e74a1ed2e9f4f578c423e74
deepakkarki <deepak6k@gmail.com>
2018-10-31 09:47:05 +0530

    added plugin for sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    `gatsby-plugin-sitemap`,
     `gatsby-mdx`, //- doesn't work :( 
     {
       resolve: `gatsby-plugin-feed`,

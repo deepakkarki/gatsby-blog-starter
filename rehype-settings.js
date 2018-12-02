199cd61e177d837b19a0a12fab2a82ce6e7029b5
deepakkarki <deepak6k@gmail.com>
2018-12-02 23:36:57 +0530

    newsletter components that I didn't end up using anyways

--- rehype-settings.js
+++ rehype-settings.js
@@ -1,7 +1,8 @@
 import rehypeReact from "rehype-react"
 import React from "react"
 import { Link } from "gatsby"
-
+import Newsletter from "./src/components/newsletter/newsletter"
+import MiniNewsletter from "./src/components/newsletter/mini-newsletter"
 function MyButton(props){
     return <button>{props.children}</button>
 }
@@ -11,6 +12,8 @@ export const renderAst = new rehypeReact({
     createElement: React.createElement,
     components: { 
       "my-button": MyButton,
-      "i-link" : Link
+      "i-link" : Link,
+      "news-letter" : Newsletter,
+      "mini-newsletter": MiniNewsletter,
     },
   }).Compiler
